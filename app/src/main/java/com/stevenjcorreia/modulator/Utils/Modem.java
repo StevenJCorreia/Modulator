36eb6a923e2527ef4b31e239d91f192bdd1e55ce
Steven Correia <stevenjosephcorreia@gmail.com>
2020-03-06 17:33:55 -0500

    Code refactor and feature addition
    Removed unnecessary variables, added parity bit to NRZ-I implementation

--- app/src/main/java/com/stevenjcorreia/modulator/Utils/Modem.java
+++ app/src/main/java/com/stevenjcorreia/modulator/Utils/Modem.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 
 /*
 * This module converts characters to binary using UTF-8.
+* This module uses even parity.
 * */
 // TODO - Add delimiters & parity bit to Manchester
 public class Modem {
@@ -32,7 +33,6 @@ public class Modem {
 
     private static float[][] Modulate(int[] bytes, int mode) {
         float[][] output = new float[bytes.length][];
-        float startingVoltage;
 
         // Initialize sub-arrays
         for (int i = 0; i < bytes.length; i++) {
@@ -45,8 +45,8 @@ public class Modem {
                     for (int j = 0; j < String.valueOf(bytes[i]).length(); j++) { // Each character's binary string char-element
                         if (j == 0) {
                             // If first bit, set starting voltage to appropriate voltage (0 = high, 1 = low)
-                            output[i][j] = startingVoltage = String.valueOf(bytes[i]).charAt(j) == '0' ? HIGH_VOLTAGE : LOW_VOLTAGE;
-                            Log.d(TAG, String.format(Locale.US, "bit %s receives voltage of %f.", String.valueOf(bytes[i]).charAt(j), startingVoltage));
+                            output[i][j] = String.valueOf(bytes[i]).charAt(j) == '0' ? HIGH_VOLTAGE : LOW_VOLTAGE;
+                            Log.d(TAG, String.format(Locale.US, "bit %s receives voltage of %f.", String.valueOf(bytes[i]).charAt(j), output[i][j]));
                             continue;
                         }
 
@@ -66,7 +66,6 @@ public class Modem {
 
     private static int[] demodulate(float[][] voltages) {
         int[] output = new int[voltages.length];
-        int startingBit;
 
         // For every voltage in every sub-array, append 0 or 1 to string, convert to int, add to respective index
         for (int i = 0; i < voltages.length; i++) {
@@ -74,9 +73,8 @@ public class Modem {
             for (int j = 0; j < voltages[i].length; j++) {
                 if (j == 0) {
                     // If first voltage, set starting bit to appropriate voltage (high = 0, low = 1)
-                    startingBit = Float.compare(voltages[i][j], HIGH_VOLTAGE) == 0 ? 0 : 1;
-                    byteValue.append(startingBit);
-                    Log.d(TAG, String.format(Locale.US, "voltage of %f receives bit %s.", voltages[i][j], startingBit));
+                    byteValue.append(Float.compare(voltages[i][j], HIGH_VOLTAGE) == 0 ? 0 : 1);
+                    Log.d(TAG, String.format(Locale.US, "voltage of %f receives bit %s.", voltages[i][j], byteValue));
                     continue;
                 }
 
@@ -98,19 +96,34 @@ public class Modem {
         }
 
         for (int i = 0; i < output.length; i++) {
-            output[i] = Integer.parseInt(Integer.toString(output[i], 2));
+            output[i] = Integer.parseInt(setParityBit(Integer.toString(output[i], 2)));
         }
 
         return output;
     }
 
+    private static String setParityBit(String byteValue) {
+        int onBitCount = 0;
+        for (int i = 0; i < byteValue.length(); i++) {
+            if (byteValue.charAt(i) == '1') {
+                onBitCount++;
+            }
+        }
+
+        return byteValue.concat((onBitCount & 1) == 0 ? "0" : "1");
+    }
+
     private static String binaryArrayToAsciiString(int[] bytes) {
         char[] ASCII = new char[bytes.length];
         for (int i = 0; i < ASCII.length; i++) {
-            ASCII[i] = (char) Integer.parseInt(String.valueOf(bytes[i]), 2);
+            ASCII[i] = (char) Integer.parseInt(stripParityBit(String.valueOf(bytes[i])), 2);
         }
 
         return String.valueOf(ASCII);
+    }
+
+    private static String stripParityBit(String byteValue) {
+        return byteValue.substring(0, byteValue.length() - 1);
     }
 
     private static float swapVoltage(float voltage) {
